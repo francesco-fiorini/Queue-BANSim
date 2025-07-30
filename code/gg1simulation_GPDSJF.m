5981d4596bea69cec95182b17c50f66b150849d5
Francesco Fiorini <francesco.fiorini@phd.unipi.it>
2025-07-30 12:48:11 +0200

    Update gg1simulation_GPDSJF.m

--- code/gg1simulation_GPDSJF.m
+++ code/gg1simulation_GPDSJF.m
@@ -4,73 +4,111 @@
 % Mail: francesco.fiorini@phd.unipi.it
 
 function [mean_delay, mean_queue_size] = gg1simulation_GPDSJF(muL_a, sigmaL_a, lambdaW_s, k_s, total_arrivals, use_factor)
-    % GG1 queue simulation with non-preemptive SJF policy using BanArray
-    
 
-    % Initialize time arrays as BanArray
-    arrival_time       = zeros(total_arrivals, 1, 'like', BanArray);   % arrival times
-    enterservice_time  = zeros(total_arrivals, 1, 'like', BanArray);   % service start times
-    departure_time     = zeros(total_arrivals, 1, 'like', BanArray);   % departure times
+    % Initialize arrays with BanArray type
+    arrival_time = zeros(total_arrivals, 1, 'like', BanArray);
+    enterservice_time = zeros(total_arrivals, 1, 'like', BanArray);
+    departure_time = zeros(total_arrivals, 1, 'like', BanArray);
+    service_time = randWeibullEuclidea(lambdaW_s, k_s, total_arrivals);
+    arrival_time_support = randLogNormEuclidea(muL_a, sigmaL_a, total_arrivals) * Ban(1,1);
 
-    % Generate service times and interarrival times
-    service_time = randWeibullEuclidea(lambdaW_s, k_s, total_arrivals);           % service time for each job (BanArray)
-    interarrival_support = randLogNormEuclidea(muL_a, sigmaL_a, total_arrivals) * Ban(1,1);
-
-    % Build cumulative arrival times
-    arrival_time(1).bArr = interarrival_support(1);
+    % Set first arrival time and generate cumulative arrival times
+    arrival_time(1).bArr = arrival_time_support(1);
     for i = 2:total_arrivals
-        arrival_time(i).bArr = arrival_time(i-1) + interarrival_support(i);
+        arrival_time(i).bArr = arrival_time(i-1) + arrival_time_support(i);
     end
 
-    % Vector to track which jobs have been scheduled
-    served = false(total_arrivals, 1);
+    % Simulation variables
+    waiting_queue = [];          % Indices of waiting jobs
+    current_time = Ban(0);       % Current simulation time
+    server_busy = false;         % Server status
+    next_departure = Ban(0);     % Time of next departure
 
-    % First job: starts service immediately upon arrival
-    enterservice_time(1).bArr = arrival_time(1);
-    departure_time(1).bArr    = enterservice_time(1) + service_time(1);
-    served(1)                  = true;
+    % Process arrivals
+    for i = 1:total_arrivals
+        % Check for departures before handling arrival
+        while server_busy && next_departure < arrival_time(i)
+            current_time = next_departure;
+            server_busy = false;
 
-    % Main loop: schedule next arrivals using non-preemptive SJF
-    for i = 2:total_arrivals
-        % Time when server becomes free (end of previous job)
-        t_free = departure_time(i-1);
-
-        if t_free <= arrival_time(i)
-            % Server is idle upon arrival: job i starts immediately
-            enterservice_time(i).bArr = arrival_time(i);
-        else
-            % Server is busy: select shortest job from queue
-            min_srv    = Ban(1, 10);   % large initial Ban value
-            selected_j = 0;
-
-            % Search among jobs that have arrived and not yet served
-            for j = 1:(i-1)
-                if arrival_time(j) <= t_free && ~served(j)
-                    if service_time(j) < min_srv
-                        min_srv    = service_time(j);
-                        selected_j = j;
+            % Serve next job from waiting queue if not empty
+            if ~isempty(waiting_queue)
+                % Find shortest service time job
+                min_service = service_time(waiting_queue(1));
+                selected_job = waiting_queue(1);
+                for k = 2:length(waiting_queue)
+                    if service_time(waiting_queue(k)) < min_service
+                        min_service = service_time(waiting_queue(k));
+                        selected_job = waiting_queue(k);
                     end
                 end
-            end
 
-            if selected_j > 0
-                % Schedule the selected job at t_free
-                enterservice_time(selected_j).bArr = t_free;
-                departure_time(selected_j).bArr    = t_free + service_time(selected_j);
-                served(selected_j)                 = true;
+                % Remove job from queue and schedule it
+                waiting_queue = waiting_queue(waiting_queue ~= selected_job);
+                enterservice_time(selected_job).bArr = current_time;
+                departure_time(selected_job).bArr = current_time + service_time(selected_job);
+                next_departure = departure_time(selected_job);
+                server_busy = true;
             end
-
-            % Schedule arrival job i to start at t_free
-            enterservice_time(i).bArr = t_free;
         end
 
-        % Compute departure time of job i and mark as served
-        departure_time(i).bArr = enterservice_time(i) + service_time(i);
-        served(i)              = true;
+        % Handle new arrival
+        if ~server_busy
+            enterservice_time(i).bArr = arrival_time(i);
+            departure_time(i).bArr = enterservice_time(i) + service_time(i);
+            next_departure = departure_time(i);
+            server_busy = true;
+        else
+            waiting_queue = [waiting_queue, i];
+        end
     end
 
-    % Compute delays and performance metrics
-    delay = departure_time - arrival_time;          % time in system for each job
-    mean_queue_size = sum(double(delay)) / double(departure_time(total_arrivals));
-    mean_delay      = mean(double(delay));
+    % Process remaining jobs in queue
+    while ~isempty(waiting_queue)
+        if server_busy
+            current_time = next_departure;
+            server_busy = false;
+        end
+
+        % Find shortest service time job
+        min_service = service_time(waiting_queue(1));
+        selected_job = waiting_queue(1);
+        for k = 2:length(waiting_queue)
+            if service_time(waiting_queue(k)) < min_service
+                min_service = service_time(waiting_queue(k));
+                selected_job = waiting_queue(k);
+            end
+        end
+
+        % Remove job from queue and schedule it
+        waiting_queue = waiting_queue(waiting_queue ~= selected_job);
+        enterservice_time(selected_job).bArr = current_time;
+        departure_time(selected_job).bArr = current_time + service_time(selected_job);
+        next_departure = departure_time(selected_job);
+        server_busy = true;
+    end
+
+    % Calculate metrics
+    delay = zeros(total_arrivals, 1, 'like', BanArray);
+    for i = 1:total_arrivals
+        delay(i).bArr = departure_time(i) - arrival_time(i);
+    end
+
+    % Sum delays
+    sum_delay = delay(1);
+    for i = 2:total_arrivals
+        sum_delay = sum_delay + delay(i);
+    end
+
+    % Find last departure
+    last_departure = departure_time(1);
+    for i = 2:total_arrivals
+        if last_departure < departure_time(i)
+            last_departure = departure_time(i);
+        end
+    end
+
+    % Compute results
+    mean_queue_size = sum_delay / last_departure;
+    mean_delay = sum_delay / total_arrivals;
 end
